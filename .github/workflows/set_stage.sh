19506fe03abd81a1febeb12aa4635b7945e1b868
Brian Torres-Gil <brian@ixi.us>
2019-11-17 07:12:46 -0800

    Create set_stage.sh

---
+++ .github/workflows/set_stage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+case "${{ github.ref }}" in
+  refs/heads/master)
+    echo "::set-env name=STAGE::prod"
+    echo "::set-output name=auth::test1"
+    ;;
+  refs/heads/staging)
+    echo "::set-env name=STAGE::staging"
+    echo "::set-output name=auth::test2"
+    ;;
+  *)
+    exit 1
+    ;;
+esac
